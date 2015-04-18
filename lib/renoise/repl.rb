8aa262154ea282b08a05c919f2cc6ab4f6669f7c
Adrian Enns <addis.aden@gmail.com>
2015-04-18 20:46:07 +0200

    setup connection

--- lib/renoise/repl.rb
+++ lib/renoise/repl.rb
@@ -5,6 +5,16 @@ module Renoise
   class Repl
     def initialize
       puts "Renoise Repl ... Bootup"
+      puts "Please be aware to Start Renoise-OSC Server in UDP-Mode"
+      @host = Readline.readline("Host: ", true)
+      @port = Readline.readline("Port: ", true).to_i
+
+      print "Setup connection ... "
+      @renoise = Renoise::Core.new(@host, @port)
+      puts "Done"
+      print "Setup BPM to 120 ... "
+      @renoise.bpm = 120
+      puts "Done"
     end
   end
 end
